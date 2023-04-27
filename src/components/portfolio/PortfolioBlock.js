010b1e50aa79c8fecc100347ed0a9eea2ed2063e
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-04-26 23:34:01 -0700

    modified the text of buttons on portfolio page

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -11,10 +11,10 @@ function PortfolioBlock(props) {
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'}/>
+               <IconLink link={live} title={'View Site'} icon={'fa fa-safari'}/>
             </Box>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
+               <IconLink link={source} title={'View Code'} icon={'fa fa-code'}/>
             </Box>
          </Box>
       </Box>
