79c48891f1ad00b526bab3127d6cc4c7839aa8b6
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-04-26 23:47:36 -0700

    added some margin and border spacing to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Style from './Navbar.module.scss';
 import Toggler from "./home/Toggler";
 import {Link, useLocation} from "react-router-dom";
 import {Box} from "@mui/material";
-import {info} from "../info/Info";
+
 
 const links = [
     {
