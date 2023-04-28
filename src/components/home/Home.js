7c7bb3fbbf93d0c81b4b616b2ff1e026f5bdefe6
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-04-27 23:18:59 -0700

    updated the navbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,12 +8,13 @@ import {Box} from "@mui/material";
 import {info} from "../../info/Info";
 import Type from "./Type";
 
+
 export default function Home() {
 
    return (
       <Box component={'main'} display={'flex'} flexDirection={{xs: 'column', md: 'row'}} alignItems={'center'}
            justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
-         <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{background: info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '40vh'}}
+            <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{background: info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '40vh'}}
               height={{xs: '35vh', md: '40vh'}}
               borderRadius={'50%'} p={'0.75rem'} mb={{xs: '1rem', sm: 0}} mr={{xs: 0, md: '2rem'}}/>
          <Box>
