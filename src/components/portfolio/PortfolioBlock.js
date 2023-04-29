6f5ec34f4f8faf9dcd1b0d066f34ca532c9d3866
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-04-29 12:23:51 -0700

    changed the flex directions of buttons on portfolio page

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -15,8 +15,8 @@ function PortfolioBlock(props) {
          <Box component={'img'} src={image} alt={'projects'}/>
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
          <h1 style={{fontSize: '1rem'}}>{description}</h1>
-         <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
-              alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
+         <Box className={'portfolio'} display={'flex'} flexDirection={'row'} gap={'0.5rem'}
+              alignItems={'center'} fontSize={'1.5rem'} py={'2rem'} justifyContent={'space-between'}>
             <Box p={1} border={'2px solid green'} borderRadius={'15px'}>
                <IconLink link={live} title={'View Site'} icon={'fa fa-link'}/>
             </Box>
