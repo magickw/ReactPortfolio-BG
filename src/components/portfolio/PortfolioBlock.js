7b0cb674f051f2f70d8eadd9a9411f5e6f7b1280
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-04-30 16:50:32 -0700

    changed the spacing on portfolio page

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -11,13 +11,15 @@ function PortfolioBlock(props) {
          flexDirection={'column'}
          justifyContent={'center'}
          alignItems={'center'}
-         p={10}
+         pr={10}
+         pl={10}
+         pt={5}
       >
          <Box component={'img'} src={image} alt={'projects'}/>
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
          <h1 style={{fontSize: '1rem'}}>{description}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'row'} gap={'0.5rem'}
-              alignItems={'center'} fontSize={'1.5rem'} py={'2rem'} justifyContent={'space-between'}>
+              alignItems={'center'} fontSize={'1.5rem'} py={'1rem'} justifyContent={'space-between'}>
             <Box p={1} border={'2px solid green'} borderRadius={'15px'}>
                <IconLink link={live} title={'View Site'} icon={'fa fa-link'}/>
             </Box>
