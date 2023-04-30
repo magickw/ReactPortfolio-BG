81f5077c10dc01bfa5d6ed2d22b5ec1d38c4bd91
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-04-30 10:57:24 -0700

    added a button  redirecting the user back to home page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
     <div className="not-found">
       <h1>404 Not Found</h1>
       <p>Sorry, the page you are looking for could not be found.</p>
+      <Link to="/">Go back to the home page</Link>
     </div>
   );
 }
