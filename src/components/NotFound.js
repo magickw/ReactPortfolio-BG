f94471fad7ec2875c5899d6dda752b34b64fb90f
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-04-30 23:26:26 -0700

    added a arrow-left favicon to the button on 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { Box, Typography } from '@mui/material';
+import { ArrowLeft as ArrowLeftIcon } from '@mui/icons-material';
 import styles from './NotFound.module.scss';
 
 function NotFound() {
   return (
-    <Box className={styles.notFound} display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
-      <Typography variant='h1' align='center'>404 Not Found</Typography>
+    <div className={styles.notFound}>
+      <Typography variant='h1' color='error' align='center'>404 Not Found</Typography>
       <Typography variant='body1' align='center'>Sorry, the page you are looking for could not be found.</Typography>
-      <Link to="/" className={styles.link}>Go back to the home page</Link>
-    </Box>
+      <div className={styles.buttonContainer}>
+        <Button
+          component={Link}
+          to="/"
+          variant='contained'
+          color='primary'
+          startIcon={<ArrowLeftIcon />}
+          className={styles.button}
+        >
+          Go back to the home page
+        </Button>
+      </div>
+    </div>
   );
 }
 
