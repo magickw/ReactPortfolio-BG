fdadc119064c61adea271e28c3ac5f7a2c8e3e68
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-05-02 10:26:05 -0700

    added zoom effect on 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,12 +3,16 @@ import { Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { ArrowLeft as ArrowLeftIcon } from '@mui/icons-material';
 import Style from './NotFound.module.scss';
+import Zoom from 'react-reveal/Zoom';
 
 export default function NotFound() {
   return (
     <div className={Style.notFound}>
-      <Typography variant='h1' color='error' align='center'>404</Typography>
-      <Typography variant='h1' color='error' align='center'>Page Not Found</Typography>
+      <Zoom top>
+        <Typography variant='h1' color='error' align='center'>404</Typography>
+        <Typography variant='h1' color='error' align='center'>Page Not Found</Typography>
+      </Zoom>
+
       <Typography variant='body1' align='center'>Sorry, the page you are looking for could not be found.</Typography>
       <div className={Style.buttonContainer}>
         <Button
