1f47e5b13faa55c4dbc41ec512a1b770ed16ef3a
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-05-03 23:08:29 -0700

    changed the delay intervals on fade effects

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,56 +1,24 @@
-// import React from 'react';
-// import { Button, Typography } from '@mui/material';
-// import { Link } from 'react-router-dom';
-// import { ArrowLeft as ArrowLeftIcon } from '@mui/icons-material';
-// import Style from './NotFound.module.scss';
-// import Zoom from 'react-reveal/Zoom';
-
-// export default function NotFound() {
-//   return (
-//     <div className={Style.notFound}>
-//       <Zoom top>
-//         <Typography variant='h1' color='error' align='center'>404</Typography>
-//         <Typography variant='h1' color='error' align='center'>Oops, Page Not Found</Typography>
-//       </Zoom>
-
-//       <Typography variant='body1' align='center'>Sorry, the page you are looking for could not be found.</Typography>
-//       <div className={Style.buttonContainer}>
-//         <Button
-//           component={Link}
-//           to="/"
-//           variant='contained'
-//           color='primary'
-//           startIcon={<ArrowLeftIcon />}
-//           className={Style.button}
-//         >
-//           Go back to the homepage
-//         </Button>
-//       </div>
-//     </div>
-//   );
-// }
-
-
 import React from 'react';
 import { Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { ArrowLeft as ArrowLeftIcon } from '@mui/icons-material';
 import Style from './NotFound.module.scss';
+import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
 
 export default function NotFound() {
   return (
     <div className={Style.notFound}>
-      <Fade>
+      <Zoom top>
         <Typography variant='h1' color='error' align='center'>404</Typography>
         <Typography variant='h1' color='error' align='center'>Oops, Page Not Found</Typography>
-      </Fade>
+      </Zoom>
 
-      <Fade delay={500}>
+      <Fade delay={1000}>
         <Typography variant='body1' align='center'>Sorry, the page you are looking for could not be found.</Typography>
       </Fade>
 
-      <Fade delay={1000}>
+      <Fade delay={1500}>
         <div className={Style.buttonContainer}>
           <Button
             component={Link}
