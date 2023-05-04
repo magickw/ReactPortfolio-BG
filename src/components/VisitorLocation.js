69856ccc4dbc929148c8bccf06c4847dc99b2a0a
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-05-04 00:36:57 -0700

    showing country flag based on the visitor's location

--- src/components/VisitorLocation.js
+++ src/components/VisitorLocation.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ReactCountryFlag from 'react-country-flag';
 
 const VisitorLocation = () => {
   const [visitorLocation, setVisitorLocation] = useState(localStorage.getItem('visitorLocation'));
@@ -8,16 +9,15 @@ const VisitorLocation = () => {
     fetch('https://ipapi.co/json/')
       .then(response => response.json())
       .then(data => {
-        const location = `${data.city}, ${data.region}, ${data.country}`;
-        setVisitorLocation(location);
-        localStorage.setItem('visitorLocation', location);
+        setVisitorLocation(data);
+        localStorage.setItem('visitorLocation', JSON.stringify(data));
       })
       .catch(error => console.error(error));
   }, []);
 
   return (
     <div>
-      <p>Your current location: {visitorLocation}</p> 
+      <p>Your current location: {visitorLocation && `${visitorLocation.city}, ${visitorLocation.region}, ${visitorLocation.country_name}`} <ReactCountryFlag countryCode={visitorLocation && visitorLocation.country_code} svg /></p> 
     </div>
   );
 };
@@ -25,36 +25,3 @@ const VisitorLocation = () => {
 export default VisitorLocation;
 
 
-// import React, { useState, useEffect } from "react";
-
-// const VisitorLocation = () => {
-//   const [location, setLocation] = useState(null);
-
-//   useEffect(() => {
-//     const fetchLocation = async () => {
-//       const response = await fetch("https://ipapi.co/json/");
-//       const data = await response.json();
-//       setLocation(data);
-//       localStorage.setItem("visitorLocation", JSON.stringify(data));
-//     };
-//     const visitorLocation = localStorage.getItem("visitorLocation");
-//     if (visitorLocation) {
-//       setLocation(JSON.parse(visitorLocation));
-//     } else {
-//       fetchLocation();
-//     }
-//   }, []);
-
-//   return (
-//     <div>
-//       {location && (
-//         <div>
-//           <span>You're visiting from {`${location.city}, ${location.region}, ${location.country_name}`}</span>
-//           <img src={`https://flagcdn.com/16x12/${location.country_code.toLowerCase()}.png`} alt={location.country_name} />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default VisitorLocation;
