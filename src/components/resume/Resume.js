c9674f898a1e7e8defbb8a727b395d3a98ded19d
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-05-04 15:12:11 -0700

    added a heading to the resume page

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -3,6 +3,8 @@ import { Box, Button } from '@mui/material';
 import MyResume from './Baofeng_Guo_Resume.pdf?raw';
 import { FaDownload } from "react-icons/fa";
 import { Document, Page, pdfjs } from "react-pdf";
+import { info } from "../../info/Info";
+import Zoom from 'react-reveal/Zoom';
 import Style from './Resume.module.scss';
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -10,7 +12,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 export default function Resume() {
   const [width, setWidth] = useState(1200);
-  
+
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -18,6 +20,28 @@ export default function Resume() {
 
   return (
     <Box>
+      <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} fontSize={'1.5rem'}>
+        <Zoom top>
+          <h1>
+            <span
+              style={{
+                background: info.gradient,
+                WebkitBackgroundClip: 'text',
+                WebkitTextFillColor: 'transparent',
+              }}
+            >
+              Expierence & Education
+            </span>
+          </h1>
+        </Zoom>
+      </Box>
+
+      <Box display="flex" justifyContent="center">
+        <Document file={MyResume} style={{ position: "relative" }}>
+          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+        </Document>
+      </Box>
+
       <Box display="flex" justifyContent="center" mb={2}>
         <Button
           href={MyResume}
@@ -29,12 +53,6 @@ export default function Resume() {
           <FaDownload />
           &nbsp;Download CV
         </Button>
-      </Box>
-
-      <Box display="flex" justifyContent="center">
-        <Document file={MyResume} style={{ position: "relative" }}>
-          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-        </Document>
       </Box>
     </Box>
   );
