6847d263cc0e7e105aabca71e0216cc0806dd247
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-05-13 15:57:10 -0700

    added vercel Analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.module.scss';
 import BaseLayout from "./components/BaseLayout";
 import {BrowserRouter} from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 
 
 
@@ -12,6 +13,7 @@ function App() {
          <BrowserRouter>
             <BaseLayout/>
          </BrowserRouter>
+         <Analytics />
       </div>
    );
 }
