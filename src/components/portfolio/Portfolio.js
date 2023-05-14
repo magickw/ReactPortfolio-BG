1290bdb526caa8fb973939840a70aee9ef0fe27b
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2023-05-14 12:25:57 -0700

    styled the show more button

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,16 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PortfolioBlock from "./PortfolioBlock";
 import { Box, Grid, Button } from "@mui/material";
 import { info } from "../../info/Info";
 import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
+import Style from './Portfolio.module.scss';
 
 
 export default function Portfolio() {
     const [showAll, setShowAll] = useState(false);
     const toggleShowAll = () => {
-    setShowAll(!showAll);
-  };
+        setShowAll(!showAll);
+    };
     return (
         <Box>
             <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} fontSize={'1.5rem'}>
@@ -30,18 +31,20 @@ export default function Portfolio() {
             </Box>
             <Fade delay={1000}>
                 <Grid container sx={{ gap: 6 }} justifyContent="center">
-                    {info.portfolio.slice(0,showAll ? info.portfolio.length : 4).map((project, index) => (
+                    {info.portfolio.slice(0, showAll ? info.portfolio.length : 4).map((project, index) => (
                         <Grid item xs={8} md={6} lg={4} key={index}>
                             <PortfolioBlock image={project.image} description={project.description} techStacks={project.techStacks} live={project.live} source={project.source} title={project.title} />
                         </Grid>
                     ))}
                 </Grid>
             </Fade>
+            <Fade delay={1000}>
             <Box display="flex" justifyContent="center" mt={2}>
-        <Button variant="outlined" onClick={toggleShowAll}>
-          {showAll ? 'Show Less' : 'Show More'}
-        </Button>
-      </Box>
+                <Button className={Style.showButton} variant="outlined" onClick={toggleShowAll}>
+                    {showAll ? 'Show Less' : 'Show More'}
+                </Button>
+            </Box>
+            </Fade>
         </Box>
     );
 };
